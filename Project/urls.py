007dea7beea3ab57552c2ce50d8d62efe3e562ba
maumg1196 <maumg1196@gmail.com>
2017-09-30 21:56:33 -0500

    First Commit

---
+++ Project/urls.py
@@ -0,0 +1,8 @@
+"""Project URL Configuration."""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^dummy/', include('dummy.urls', namespace='dummy')),
+]
